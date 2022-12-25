9e27606ab6eb03bd4db735654eb6d2eb393f3b44
Давыдов Вадим Борисович <v.b.davydov@urfu.me>
2022-12-25 14:17:35 +0500

    conflict

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ def stat_vac():
     """
     main_input_request = "Выберите тип вывода: "
     main_input_info = input(main_input_request)
-    if main_input_info != "Вакансии" and main_input_info != "Статистика":
+    if main_input_info != "Вакансии" and main_input_info != "Вакансии":
         print("Введён неправильный тип вывода")
         return
-    if main_input_info == "Вакансии":
+    if main_input_info == "Навык":
         get_vacancies()
     else:
         get_statistics()
-    # get_statistics()
 
 if __name__ == '__main__':
     stat_vac()
