2866cd7d2a7124e79220df4d8abe452ea3766e15
Давыдов Вадим Борисович <v.b.davydov@urfu.me>
2022-12-25 13:11:08 +0500

    2.2.2

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from statistics import get_statistics
+from vacancy import get_vacancies
 
+def stat_vac():
+    """Выбор типа анализа данных из csv-файла
+    """
+    main_input_request = "Выберите тип вывода: "
+    main_input_info = input(main_input_request)
+    if main_input_info != "Вакансии" and main_input_info != "Статистика":
+        print("Введён неправильный тип вывода")
+        return
+    if main_input_info == "Вакансии":
+        get_vacancies()
+    else:
+        get_statistics()
+    # get_statistics()
+
+if __name__ == '__main__':
+    stat_vac()
